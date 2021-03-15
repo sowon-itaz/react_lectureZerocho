fc5fe059210e9f3590681c6e7a09a53098e87919
sowon <sowon@itaz.kr>
2021-03-15 10:31:51 +0900

    RPS ver Class

---
+++ RPS/client.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import RPSClass from "./RPSClass";
+
+ReactDOM.render(<RPSClass />, document.querySelector("#root"));
---
+++ RPS/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = {
+  name: "RPS-dev",
+  mode: "development", //개발중일때는 : development 실서비스에서는 production
+  devtool: "eval", // 개발중일때는: eval 실서비스: hidden-source-map
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  //entry에 파일을 읽고 modules를 적용한 뒤 output으로 파일을 합쳐서 하나로 만든다.
+  entry: {
+    app: "./client",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: { browsers: ["last 2 chrome versions"] },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-refresh/babel",
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [new RefreshWebpackPlugin()],
+  output: {
+    path: path.join(__dirname, "dist"), //__dirname는 현재폴더(c://블라블라/dist)
+    filename: "app.js", //entry의 app안에 파일들을 합쳐서 app.js로 만들어 주는것
+    publicPath: "/dist",
+  },
+
+  devServer: {
+    publicPath: "/dist",
+    hot: true,
+  },
+};
